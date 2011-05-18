ce7441681ec1c3c43970ed4026ce8e4b1576a158
teenst <teens.twenties@gmail.com>
2011-05-19 01:48:11 +0900

    make main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "parser.h"
+
+int main(){
+  struct Object *env = make_env();
+  struct Object *symbol=make_sym("a");
+  struct Object *number=make_num(1);
+  env_set(env,symbol,number);
+  
+  read_eval_print_loop(stdin,env);
+
+  
+  return 0;
+}
+
--- parser.c
+++ parser.c
@@ -7,8 +7,6 @@
 #define BUFFER_SIZE 256
 
 
-
-
 struct Object *make_num(int num){
   struct Object *object;
   object = malloc(sizeof(struct Object));
@@ -246,7 +244,7 @@ void read_eval_print_loop(FILE *fp,struct Object *env){
   while((buf=getc(fp))!=EOF){
     ungetc(buf,fp);
     print_object(eval(parse_sexp(fp),env));
-    
+#    
   }
 }
 
@@ -266,16 +264,4 @@ struct Object *parse_program(FILE *fp){
   }
 }
 
-
-int main(){
-  struct Object *env = make_env();
-  struct Object *symbol=make_sym("a");
-  struct Object *number=make_num(1);
-  env_set(env,symbol,number);
-  
-  read_eval_print_loop(stdin,env);
-
-  
-  return 0;
-}
 
